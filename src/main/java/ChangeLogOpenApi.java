5182242476286d6eaf8b2ed17dbe615556c2aa83
Lucas-Silva75 <lucas.lima@sensedia.com>
2022-07-01 10:31:37 -0300

    commit inicial

---
+++ src/main/java/ChangeLogOpenApi.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.flipkart.zjsonpatch.DiffFlags;
+import com.flipkart.zjsonpatch.JsonDiff;
+import com.google.gson.Gson;
+import model.DiffResponse;
+import model.ResultLog;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+public class ChangeLogOpenApi {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+        JsonNode file1 = objectMapper.readTree(
+                new File("/home/lucaslima/Downloads/accounts-1.0.3.yml"));
+
+        JsonNode file2 = objectMapper.readTree(
+                new File("/home/lucaslima/Downloads/accounts-2.0.0.yml"));
+
+        JsonNode patch = JsonDiff.asJson(
+                file1,
+                file2,
+                EnumSet.of(DiffFlags.ADD_ORIGINAL_VALUE_ON_REPLACE));
+
+        String diffs = patch.toString();
+		System.out.println(diffs);
+    }
+}
